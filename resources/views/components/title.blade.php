ee044d8961c4b12a576c1717525312934abfea2f
vladyslavlopatin <ilopatin3@gmail.com>
2023-04-30 14:06:43 +0200

    strict-title fixed

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,5 +1,5 @@
 @isset($strictTitle)
-    @section('title', config('app.name') . ': ' . $strictTitle))
+    @section('title', config('app.name') . ': ' . $strictTitle)
 @else
     @section('title', config('app.name') . ': ' . __("laravel-seo::seo.$title", $params ?? []))
 @endisset
