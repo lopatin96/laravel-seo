dc8b7ba62d5a3254ae1cf19acccb38fcf57ba160
vladyslavlopatin <ilopatin3@gmail.com>
2023-04-30 14:05:18 +0200

    strict-title

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,1 +1,5 @@
-@section('title', config('app.name') . ': ' . __("laravel-seo::seo.$title", $params ?? []))
+@isset($strictTitle)
+    @section('title', config('app.name') . ': ' . $strictTitle))
+@else
+    @section('title', config('app.name') . ': ' . __("laravel-seo::seo.$title", $params ?? []))
+@endisset
