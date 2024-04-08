a2b59f41125e80915885727087f62d1534c5582b
vladyslavlopatin <ilopatin3@gmail.com>
2024-04-08 23:23:12 +0200

    og:image

--- resources/views/components/seo.blade.php
+++ resources/views/components/seo.blade.php
@@ -1,2 +1,3 @@
 <title>@yield('title', __('laravel-seo::seo.default_title'))</title>
 <meta name="description" content="@yield('description', __('laravel-seo::seo.default_description'))">
+<meta property="og:image" content="{{ asset('og-images/en.png') }}">
