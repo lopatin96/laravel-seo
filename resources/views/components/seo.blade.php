1e639c22971d727728f78b33a19abd95f2ec95d7
vladyslavlopatin <ilopatin3@gmail.com>
2024-12-10 19:54:58 +0100

    remove og titles

--- resources/views/components/seo.blade.php
+++ resources/views/components/seo.blade.php
@@ -1,8 +1,2 @@
 <title>@yield('title', __('laravel-seo::seo.default_title'))</title>
 <meta name="description" content="@yield('description', __('laravel-seo::seo.default_description'))">
-<meta property="og:title" content="@yield('title', __('laravel-seo::seo.default_title'))">
-<meta property="og:description" content="@yield('description', __('laravel-seo::seo.default_description'))">
-<meta property="og:image" content="{{ asset('og-images/en.png') }}">
-<meta property="og:image:width" content="1200">
-<meta property="og:image:height" content="628">
-<meta property="og:image:type" content="image/png">
